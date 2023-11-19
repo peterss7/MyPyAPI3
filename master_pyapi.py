97fef815d91450f151eadc7389890a53922b9630
peterss7 <peterss7405926@gmail.com>
2023-11-19 17:13:38 -0500

    improved rnd img functionality

--- master_pyapi.py
+++ master_pyapi.py
@@ -6,15 +6,43 @@ import random
 import shutil
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 base_dir: str = "C:/...../"
 app = Flask(__name__)
 api = Api(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
 def directory_exists(cur_dir):
     return os.path.exists(cur_dir) and os.path.isdir(cur_dir)
+
+
+def get_rand_from_dir_tree(search_dir):
+    try:
+        focused_dir = f'{base_dir}{search_dir}'
+        subdirs = [f'{focused_dir}/{dir}' for dir in os.listdir(focused_dir)]
+        subdir_num = len(os.listdir(focused_dir))
+
+        isSearching = True
+        dir_index = 0
+        image_result = ''
+        while isSearching:
+            dir_index = random.randint(0, subdir_num - 1)
+            if len(os.listdir(subdirs[dir_index])) > 0:
+                subdir = subdirs[dir_index]
+                subdir_num = len(os.listdir(subdir))
+                subdir_index = random.randint(0, subdir_num - 1)
+                selection = os.listdir(subdir)[subdir_index]
+                selection_path = os.path.join(subdir, selection)
+                isFile = os.path.isfile(selection_path)
+                if isFile:
+                    isSearching = False
+                    image_result = send_from_directory(subdir, selection)
+        return image_result
+    except Exception as e:
+        return None
 
 
 class SpecificImg(Resource):
@@ -30,7 +58,7 @@ class SpecificImg(Resource):
                     return send_from_directory(cur_dir, img_filename)
         except FileNotFoundError:
             return {"ERROR": "File not found."}, 404
-        
+
 
 class DirectorySize(Resource):
     @staticmethod
@@ -88,7 +116,8 @@ class GetSubDirs(Resource):
     def get():
         return [directory for directory in os.listdir(base_dir) if
                 os.path.isdir(os.path.join(base_dir, directory))]
-    
+
+
 class GetRandomImage(Resource):
     @staticmethod
     def get():
@@ -100,14 +129,36 @@ class GetRandomImage(Resource):
         except Exception as e:
             return {e}, 404
 
+class GetRandomImageDeep(Resource):
+    @staticmethod
+    def get():
+        try: 
+            result = get_rand_from_dir_tree(dir)
+            
+            if result is not None:
+                return  result
+            else:
+                raise RandomImageException('Failed to find random image')
+        except RandomImageException as e:
+            return {e}, 404
+        
+        
+class RandomImageException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
-api.add_resource(SpecificImg, '/api/img/<directory>/<int:img_index>', strict_slashes=False)
+
+api.add_resource(
+    SpecificImg, '/api/img/<directory>/<int:img_index>', strict_slashes=False)
 api.add_resource(DirectorySize, '/api/data/<directory>', strict_slashes=False)
-api.add_resource(FileMover, '/api/move-img/<directory>/<int:img_index>', strict_slashes=False)
-api.add_resource(DeleteImg, '/api/delete-img/<directory>/<int:img_index>', strict_slashes=False)
+api.add_resource(
+    FileMover, '/api/move-img/<directory>/<int:img_index>', strict_slashes=False)
+api.add_resource(
+    DeleteImg, '/api/delete-img/<directory>/<int:img_index>', strict_slashes=False)
 api.add_resource(GetDirValidity, '/api/valid_dir/<directory>', strict_slashes=False)
 api.add_resource(GetSubDirs, '/api/info/', strict_slashes=False)
 api.add_resource(GetRandomImage, '/api/rnd-img', strict_slashes=False)
+api.add_resource(GetRandomImageDeep, 'api/rnd-img-deep/<directory>', strict_slashes=False)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.xx.xx', port=5500)
+    app.run(debug=True, host='192.168.86.26', port=5500)
